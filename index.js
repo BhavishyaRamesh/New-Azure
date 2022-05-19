e5fd5de5f21e08f5a417584a63db23f53f519c99
BhavishyaRamesh <101257695+BhavishyaRamesh@users.noreply.github.com>
2022-05-19 15:06:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/',(req,res)=>res.send('Hello Node 11'));
+app.get('/',(req,res)=>res.send('Hello'));
 app.listen(process.env.PORT || 3000)
